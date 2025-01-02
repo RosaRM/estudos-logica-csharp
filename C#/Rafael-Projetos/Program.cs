b080b05a08405e19f767b52c12d92e2e2f91e118
RosaRM <143812403+RosaRM@users.noreply.github.com>
2025-01-02 19:16:19 -0300

    Nova pasta

---
+++ C#/Rafael-Projetos/Program.cs
@@ -0,0 +1,84 @@
+﻿using Rafael_Projetos.Models;
+
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(5,20);
+calc.Subtrair(34, 45);
+calc.Mutiplicar(43, 6);
+calc.Dividir(42, 12);
+calc.Potencia(23, 3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*DateTime dataAtual = DateTime.Now;
+
+string apresentacao = "Chama, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.710;
+
+decimal preco = 1.80M;
+
+bool condicao = false;
+
+Console.WriteLine (apresentacao);
+
+Console.WriteLine ("Valor da váriavel quantidade: " + quantidade);
+Console.WriteLine ("Valor da váriavel altura: " + altura.ToString("0.000"));
+Console.WriteLine ("Valor da váriavel preco: " + preco);
+Console.WriteLine ("Valor da váriavel condicao: " + condicao);
+Console.WriteLine ("Valor da váriavel Data Atual: " + dataAtual);
+*/
+
+
+
+
+
+
+
+
+
+/*
+Pessoa pessoaSabida = new Pessoa();
+
+pessoaSabida.Idade = 22;
+pessoaSabida.Nome = "Rafael";
+
+pessoaSabida.Apresentar();*/
