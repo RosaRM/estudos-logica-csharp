84d8592ebac676990718cf06a1c64f8bc981a748
RosaRM <143812403+RosaRM@users.noreply.github.com>
2025-01-02 21:12:37 -0300

    Melhorando a calculadora com a classe Math

--- C#/Rafael-Projetos/Program.cs
+++ C#/Rafael-Projetos/Program.cs
@@ -1,51 +1,73 @@
-﻿using Rafael_Projetos.Models;
+﻿using System.ComponentModel;
+using Rafael_Projetos.Models;
+
+int soma = 0, numero = 0;
 
 
-Calculadora calc = new Calculadora();
 
+
+/*
+do
+{
+
+    Console.WriteLine("Digite um número (0 para parar)");
+    numero = Convert.ToInt32(Console.ReadLine());
+
+    soma += numero;
+}while(numero != 0);
+
+    Console.WriteLine($"Esta é a soma realizada {soma}");*/
+
+
+/*
+int numero = 10;
+int contador = 0;
+
+while (contador <= 10 ){
+    
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+    contador++;
+}
+*/
+
+/*
+for(int contador =0; contador <= 10; contador++)
+{
+
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+
+}
+*/
+
+
+/* Calculadora calc = new Calculadora();
+
+
+calc.RaizQuadrada(2025);*/
+//calc.Seno(30);
+//calc.Coseno(30);
+//calc.Tangente(30);
+//calc.Potencia(23, 3);
+/*
 calc.Somar(5,20);
 calc.Subtrair(34, 45);
 calc.Mutiplicar(43, 6);
 calc.Dividir(42, 12);
-calc.Potencia(23, 3);
+*/
+/*
+int numero = 10;
+int num = 10;
 
+Console.WriteLine(numero);
 
+Console.WriteLine("Incrementando o 10");
+numero++;
 
+Console.WriteLine("Decrementando o 10");
+num--;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Console.WriteLine(num);
+*/
 /*DateTime dataAtual = DateTime.Now;
 
 string apresentacao = "Chama, seja bem vindo";
@@ -66,14 +88,6 @@ Console.WriteLine ("Valor da váriavel preco: " + preco);
 Console.WriteLine ("Valor da váriavel condicao: " + condicao);
 Console.WriteLine ("Valor da váriavel Data Atual: " + dataAtual);
 */
-
-
-
-
-
-
-
-
 
 /*
 Pessoa pessoaSabida = new Pessoa();
